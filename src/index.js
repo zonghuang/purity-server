0971a8eb263a2f550d01e79bf191a37d7efe8a3f
zonghuang <1683766027@qq.com>
2022-05-04 22:00:33 +0800

    test: 测试

--- src/index.js
+++ src/index.js
@@ -23,8 +23,8 @@
 
 const http = require('http');
 
-const hostname = '127.0.0.1';
-const port = 3000;
+const hostname = 'zonghuang.cn';
+const port = 9998;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
