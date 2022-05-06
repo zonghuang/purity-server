f8400e26c84e91f9c30e7cfafba2c617a8fd8618
zonghuang <1683766027@qq.com>
2022-05-06 12:56:11 +0800

    hello

--- src/index.js
+++ src/index.js
@@ -29,7 +29,7 @@ const port = 9000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
+  res.end('Hello World!');
 });
 
 server.listen(port, hostname, () => {
